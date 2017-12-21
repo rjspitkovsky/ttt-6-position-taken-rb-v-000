7ff628493ac15cb7ed65cf410f1360d44ef27401
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-12-21 19:42:10 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
     #false
     if board[index] == "X" || board[index] == "O"
     true
-  elsif board[index] == " " || ""
-    false 
+  elsif board[index] == " " || "" || nil 
+    false
   end
 end
